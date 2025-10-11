77f1a906baa5fb9ee41b078b01af5433b7d2b2be
irfanhossain909 <irfanhpssain@gmail.com>
2025-10-11 14:44:46 +0600

    all done without splash screen

--- lib/features/splash/splash_screen.dart
+++ lib/features/splash/splash_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:lottie/lottie.dart';
 import 'package:testemu/core/component/image/common_image.dart';
 import 'package:testemu/core/config/route/app_routes.dart';
 import 'package:testemu/core/constants/app_images.dart';
@@ -14,24 +17,36 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
-  void initState() {
-    Future.delayed(const Duration(seconds: 3), () {
-      // if (LocalStorage.isLogIn) {
-      //   if (LocalStorage.myRole == 'consultant') {
-      //     Get.offAllNamed(AppRoutes.doctorHome);
-      //   } else {
-      //     Get.offAllNamed(AppRoutes.patientsHome);
-      //   }
-      // } else {
-      Get.offAllNamed(AppRoutes.onboarding);
-    });
-    super.initState();
-  }
-
+  // void initState() {
+  //   Future.delayed(const Duration(seconds: 3), () {
+  //     // if (LocalStorage.isLogIn) {
+  //     //   if (LocalStorage.myRole == 'consultant') {
+  //     //     Get.offAllNamed(AppRoutes.doctorHome);
+  //     //   } else {
+  //     //     Get.offAllNamed(AppRoutes.patientsHome);
+  //     //   }
+  //     // } else {
+  //     Get.offAllNamed(AppRoutes.onboarding);
+  //   });
+  //   super.initState();
+  // }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CommonImage(imageSrc: AppImages.noImage, size: 70).center,
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Lottie.asset(
+          // 'assets/animations/confetti.json',
+          'assets/animations/creepyshorts_splash_screen.json',
+          frameRate: FrameRate(120),
+          width: 200,
+          height: 200,
+
+          // width: MediaQuery.of(context).size.width, // বা নির্দিষ্ট width
+          // height: MediaQuery.of(context).size.height, // বা নির্দিষ্ট height
+          fit: BoxFit.contain,
+        ),
+      ),
     );
   }
 }
