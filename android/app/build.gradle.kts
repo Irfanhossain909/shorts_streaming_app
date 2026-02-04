edd11fa7802d37cc3a0cc8103d15c1568283931f
Irfan <irfan@Mds-Mac-mini-2.local>
2026-02-04 10:07:43 +0600

    app upload in new apple store

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.victorsaulmendozamena.creepyshorts"
+    namespace = "com.rubengalindo.creepyshorts"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.victorsaulmendozamena.creepyshorts"
+        applicationId = "com.rubengalindo.creepyshorts"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
